3380f089e65f3dcd4fe17fefb9e0fc791ae8ef5d
niaemani <niaemanidickson@gmail.com>
2021-10-22 11:58:20 -0400

    installed reouter dom, added nav content

---
+++ nia-emani-portfolio/src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <div>
+      <Link to="/">
+        <img
+          className="portfolio-logo"
+          src="https://lh3.googleusercontent.com/vxORRw3d_UrsZtqkZc_jxFQsNgFMrK1a6JCxTxQYJm1vwIt-0giyAbnyNXQKNEYZtfjYWWIv8mNtxgKS7Cv5fNZ3syxrp5WcYBSzfM8P16_70KOM_7C-zIKcYedwDqgt50nNcNDNcAc=h55"
+        />
+      </Link>
+      <p>About</p>
+      <p>Projects</p>
+      <p>Resume</p>
+      <p>Contact</p>
+    </div>
+  );
+}
