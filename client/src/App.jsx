937e48f444bdff7e7e4d913f269269bc14b5971d
niaemani <niaemanidickson@gmail.com>
2022-01-30 17:15:35 -0500

    installed bootstrap, popper, and jquery

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import "./App.css";
 import Layout from "./components/Layout";
 import { Switch, Route } from "react-router-dom";
 
+// Import Bootstrap CSS and JS
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
 function App() {
   return (
     <div className="App">
