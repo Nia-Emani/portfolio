5ea6b2d8dc7949ab44d9e064778a52d25b8906c2
niaemani <niaemanidickson@gmail.com>
2022-01-31 11:48:41 -0500

    updated bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 import Layout from "./components/Layout";
 import { Switch } from "react-router-dom";
 
-import "bootstrap";
 import { baseURL, config } from "./services";
 
 console.log(`$(process.env.REACT_APP_PORFOLIO_APP)`);
